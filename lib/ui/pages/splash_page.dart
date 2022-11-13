b8a82f4ea1da7001ae190daf4864889cf13ac351
Rezza <rezaseptiansyah5@gmail.com>
2022-11-14 02:29:56 +0700

    update

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -24,12 +24,25 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primaryColor,
-      body: Center(
-        child: Text(
-          'Splash Page',
-          style: whiteTextStyle,
-        ),
+      backgroundColor: whiteColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.network(
+              'https://i.pinimg.com/736x/7d/8c/f5/7d8cf585d8b703308366b207e1ba51f4.jpg',
+              fit: BoxFit.cover,
+              width: 300,
+            ),
+          ),
+          Text(
+            'Furniture Shop Fugi',
+            style: blackTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: medium,
+            ),
+          ),
+        ],
       ),
     );
   }
